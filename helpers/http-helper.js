e00e5928e3520c0ecb6f24e5b282c911887a8671
Jason Z <jason.zissman@gmail.com>
2019-11-23 16:16:13 -0600

    throttle max number http requests

--- helpers/http-helper.js
+++ helpers/http-helper.js
@@ -1,37 +1,56 @@
 const https = require("https");
+const logger = require("./log-helper");
+
+const _maxConcurrentRequests = 5;
+const _checkConcurrentRequestsFreq = 250;
+let _numberOpenRequests = 0;
 
 module.exports = {
-    issueHttpRequest: function (params) {
 
-        return new Promise(function (resolve, reject) {
+    
+    issueHttpRequest: (params) => {
 
-            var req = https.request(params, function (res) {
+        return new Promise((resolve, reject) => {            
+
+            var req = https.request(params, (res) => {
+
 
                 if (res.statusCode < 200 || res.statusCode >= 300) {
+                    _numberOpenRequests -=1;
+
                     return reject(new Error('statusCode=' + res.statusCode));
                 }
 
                 var body = [];
-                res.on('data', function (chunk) {
+                res.on('data', (chunk) => {
                     body.push(chunk);
                 });
 
-                res.on('end', function () {
+                res.on('end', () => {
+                    _numberOpenRequests -=1;
                     try {
                         body = JSON.parse(Buffer.concat(body).toString());
                     } catch (e) {
-                        console.log("Failed to parse JSON response");
+                        logger.log("Failed to parse JSON response");
                         reject(e);
                     }
                     resolve(body);
                 });
             });
 
-            req.on('error', function (err) {
+            req.on('error', (err) => {
+                _numberOpenRequests -=1;
                 reject(err);
             });
 
-            req.end();
+            let httpRequestCheckStatusIntervalId = setInterval(() => {
+                if (_numberOpenRequests < _maxConcurrentRequests) {
+                    _numberOpenRequests += 1;
+                    clearInterval(httpRequestCheckStatusIntervalId);                    
+                    logger.log("Issuing HTTP request " + params.method + " " + params.host + ":" + params.port + params.path);
+                    req.end();
+                }
+            }, _checkConcurrentRequestsFreq);
         });
     }
 };
--- helpers/timestamp-helper.js
+++ helpers/timestamp-helper.js
@@ -1,7 +1,9 @@
 module.exports = {
     fullTimestamp: function() {
-        // 11/20/2019, 10:19:09 PM CST
-        return new Date().toLocaleString( 'sv', { timeZoneName: 'short' } );
+        // 2019-11-23 16:13:30:696
+        let now = new Date();
+        return now.getFullYear() + "-" + (now.getMonth() + 1) + "-" + now.getDate() + 
+            " " +now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds() + ":" + now.getMilliseconds();
     },
     dateOnlyTimestamp: function() {
         // 2019-11-20
