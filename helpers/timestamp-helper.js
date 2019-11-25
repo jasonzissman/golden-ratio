a6db1c7c374c265d076f313a27d70be9b00b5cdd
Jason Z <jason.zissman@gmail.com>
2019-11-25 12:29:57 -0600

    Formatting milliseconds to always 3 digits

--- helpers/timestamp-helper.js
+++ helpers/timestamp-helper.js
@@ -3,7 +3,8 @@ module.exports = {
         // 2019-11-23 16:13:30:696
         let now = new Date();
         return now.getFullYear() + "-" + (now.getMonth() + 1) + "-" + now.getDate() + 
-            " " +now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds() + ":" + now.getMilliseconds();
+            " " +now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds() + ":" + 
+            String(now.getMilliseconds()).padStart(3, '0'); 
     },
     dateOnlyTimestamp: function() {
         // 2019-11-20
