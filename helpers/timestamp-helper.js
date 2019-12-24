e2fe4cf047b0bbf468a1d2833e006c684e0811d2
Jason Z <jason.zissman@gmail.com>
2019-12-24 06:19:50 -0600

    fixing padding

--- helpers/timestamp-helper.js
+++ helpers/timestamp-helper.js
@@ -2,7 +2,9 @@ module.exports = {
     fullTimestamp: function() {
         // 2019-11-23 02:03:09:696
         let now = new Date();
-        return now.getFullYear() + "-" + (now.getMonth() + 1) + "-" + now.getDate() + " " +
+        return now.getFullYear() + "-" + 
+        String(now.getMonth() + 1).padStart(2, '0') + "-" +
+        String(now.getDate()).padStart(2, '0') + " " +
             String(now.getHours()).padStart(2, '0') + ":" + 
             String(now.getMinutes()).padStart(2, '0') + ":" +
             String(now.getSeconds()).padStart(2, '0') + ":" +              
@@ -11,6 +13,8 @@ module.exports = {
     dateOnlyTimestamp: function() {
         // 2019-11-20
         let now = new Date();
-        return now.getFullYear() + "-" + (now.getMonth() + 1) + "-" + now.getDate();
+        return now.getFullYear() + "-" + 
+        String(now.getMonth() + 1).padStart(2, '0') + "-" +
+        String(now.getDate()).padStart(2, '0')
     }
 };
